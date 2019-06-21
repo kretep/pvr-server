9290c4fe09d70fab3b0bab8a801da3e81ac999f2
Peter <kretep@users.noreply.github.com>
2019-06-21 22:41:14 +0200

    Server only

--- server.js
+++ server.js
@@ -8,11 +8,20 @@ if (typeof String.prototype.startsWith != 'function') {
 var http = require('http');
 var httpProxy = require('http-proxy');
 var static = require('node-static');
+var path = require('path');
 
 var proxy = httpProxy.createProxyServer();
-var fileServer = new static.Server('./app/');
+var fileServer = new static.Server(path.join(__dirname, 'app/'));
 
-http.createServer(function (req, res) {
+proxy.on('error', function(err, req, res) {
+  console.log('proxy error: ', err);
+  res.writeHead(500, { 
+    'Content-Type': 'text/plain'
+  });
+  res.end('Proxy error: ' + err);
+});
+
+let server = http.createServer(function (req, res) {
 
   // Log debug info
   var debug = {
@@ -33,12 +42,21 @@ http.createServer(function (req, res) {
       proxy.web(req, res, {
         target: 'http://localhost:8080'
       });
-    }, 2000);
+    }, 1);
   }
   else {
-    //fileServer.serve(req, res);
-    proxy.web(req, res, {
+    fileServer.serve(req, res);
+    /*proxy.web(req, res, {
       target: 'http://localhost:3000'
-    });
+    });*/
   }
-}).listen(9090); // Some ports (e.g. 80) may require admin privileges
+})
+
+server.on('error', function(err, req, res) {
+  res.writeHead(500, { 
+    'Content-Type': 'text/plain'
+  });
+  res.end('An error occurred:' + err);
+})
+
+server.listen(9090); // Some ports (e.g. 80) may require admin privileges
